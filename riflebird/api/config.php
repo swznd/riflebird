fa252d86d983db3ac7d337f94ffc16931b67ef2b
Muhammad Fikri <fikswznd@gmail.com>
2013-10-20 23:00:56 +0700

    find config by multidimensional key

--- riflebird/api/config.php
+++ riflebird/api/config.php
@@ -19,11 +19,31 @@ class Config
     $sites = Yaml::parse($file);
     
     if ( ! empty($key)) {
+      
+      if (strpos($key, '.') !== false) {
+        $arrkeys = explode('.', $key);
+        $lenkeys = count($arrkeys);
+        $poskeys = 0;
+        
+        $sitesIt = new \RecursiveIteratorIterator(new \RecursiveArrayIterator($sites), \RecursiveIteratorIterator::SELF_FIRST);
+ 
+        foreach($sitesIt as $key => $sub) {
+          if ($key === $arrkeys[$poskeys]) {
+            if ($poskeys == $lenkeys - 1) {
+              return $sub;
+            }
+            $poskeys++;
+          }
+        }
+        
+        return null;
+      }
+      
       if ( ! empty($sites[$key])) {
         return $sites[$key];
       }
       
-      return false;
+      return null;
     }
     
     return $sites;
