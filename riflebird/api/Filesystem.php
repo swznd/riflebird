e962d20b9be1c70a3065966ff98089428f9ad0e8
Muhammad Fikri <fikswznd@gmail.com>
2013-10-29 14:57:22 +0700

    add API function (isHTML, and getFileName)

--- riflebird/api/Filesystem.php
+++ riflebird/api/Filesystem.php
@@ -8,7 +8,15 @@ class Filesystem
     return pathinfo($file, PATHINFO_EXTENSION) == 'yaml';
   }
   
+  public static function isHTML($file) {
+    return pathinfo($file, PATHINFO_EXTENSION) == 'html';
+  }
+  
   public static function isHasDir($file) {
     return dirname($file) != '.';
+  }
+  
+  public static function getFileName($file) {
+    return pathinfo($file, PATHINFO_FILENAME);
   }
 }
