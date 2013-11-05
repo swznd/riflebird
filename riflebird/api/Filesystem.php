50246ed3e02b28ea5f041b45885a0469a5abe882
Muhammad Fikri <fikswznd@gmail.com>
2013-11-05 16:38:43 +0700

    add new Filesystem API for get parentPath and readDirectory

--- riflebird/api/Filesystem.php
+++ riflebird/api/Filesystem.php
@@ -19,4 +19,30 @@ class Filesystem
   public static function getFileName($file) {
     return pathinfo($file, PATHINFO_FILENAME);
   }
+  
+  public static function parentPath($path = '') {
+    return getcwd() . '/' . $path;
+  }
+  
+  public static function readDir($dir, $filter = array()) {
+    if ( ! is_dir($dir)) {
+      return null;
+    }
+    
+    $files = array();  
+    $scanIt = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($dir));
+    foreach($scanIt as $file) {
+      if ($file->getBasename() == '.' OR $file->getBasename() == '..') {
+        continue;
+      }
+      
+      if( ! empty($filter) && ! preg_match('/'.$filter.'/', $file->getBasename())) {
+        continue;
+      }
+      
+      $files[] = str_replace($dir . '/', '', $file->getPathname());
+    }
+    
+    return $files;
+  }
 }
